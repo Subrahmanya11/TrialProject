9479d1d76ee3288aed444ba81dfbfaf165bed83b
Subrahmanya11 <agastyasince0000@gmail.com>
2022-10-13 17:50:28 +0530

    First commit

---
+++ IfElse.java
@@ -0,0 +1,23 @@
+
+public class IfElse 
+{
+
+	public static void main(String[]args)
+	{
+		int n=0;
+		 
+		if (n==0)
+		{
+		 System.out.println("Nothing");
+	     System.out.println("Bye Bye");
+		}
+
+		else if(n%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+	
+	
+}	
+			
