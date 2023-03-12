9b938559637f34224de789b92b2e6dfa85281091
Subrahmanya11 <agastyasince0000@gmail.com>
2023-03-12 22:41:36 +0530

    Updated World to India

--- FirstCode.java
+++ FirstCode.java
@@ -3,7 +3,7 @@ public class FirstCode
 {
   public static void main(String[] args)
   {
-	  System.out.println("Hello World");
+	  System.out.println("Hello India");
     System.out.print("Have a Nice Day");
   }
 }
